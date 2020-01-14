9b52dfce7a91a163a764c13b0eb7e925248af8f6
Adam Mak <adammak137@gmail.com>
2020-01-14 17:13:50 -0500

    Added more products

--- script/seed.js
+++ script/seed.js
@@ -15,25 +15,94 @@ async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const Products = await Promise.all([
-    Product.create({
-      name: 'egg',
-      price: 10.23,
-      description: 'The most eggy of them all',
-      image:
-        'https://cdn.vox-cdn.com/thumbor/CyGR5jAu364WZtRApk3UXbQd-oE=/0x0:876x584/920x613/filters:focal(368x222:508x362):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/62841097/instagram_egg.0.jpg',
-      stock: 23,
-      origin: 'USA',
-      categoryId: 1
-    })
-  ])
-
   const Categories = await Promise.all([
     Category.create({
       type: 'Fantasy'
     }),
     Category.create({
       type: 'Edible'
+    })
+  ])
+
+  const Products = await Promise.all([
+    Product.create({
+      name: 'egg',
+      price: 10.23,
+      description: 'The most eggy of them all.',
+      image:
+        'https://cdn.vox-cdn.com/thumbor/CyGR5jAu364WZtRApk3UXbQd-oE=/0x0:876x584/920x613/filters:focal(368x222:508x362):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/62841097/instagram_egg.0.jpg',
+      stock: 23,
+      origin: 'USA',
+      categoryId: 2
+    }),
+    Product.create({
+      name: 'Humpty Dumpty',
+      price: 234.99,
+      description: 'Rest assured, this eggs has not had a great fall. ',
+      image:
+        'https://www.thevintagenews.com/wp-content/uploads/2018/02/humpty_dumpty.jpg',
+      stock: 5,
+      origin: 'UK',
+      categoryId: 1
+    }),
+    Product.create({
+      name: 'Gudetama',
+      price: '.99',
+      description: 'The laziest egg there is.',
+      image:
+        'https://cdn.vox-cdn.com/thumbor/CyGR5jAu364WZtRApk3UXbQd-oE=/0x0:876x584/920x613/filters:focal(368x222:508x362):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/62841097/instagram_egg.0.jpg',
+      stock: 88,
+      origin: 'Japan',
+      categoryId: 1
+    }),
+    Product.create({
+      name: 'Kinder Surprise',
+      price: 5,
+      description: 'A surprise that might kill you.',
+      image:
+        'https://www.sltrib.com/resizer/uO0oB5WHrEjnutm_xg5hmZJfUpQ=/970x0/filters:quality(100)/arc-anglerfish-arc2-prod-sltrib.s3.amazonaws.com/public/LNXMEMHN2BEFHE53JTBS4FRFCE.jpg',
+      stock: 33,
+      origin: 'UK',
+      categoryId: 2
+    }),
+    Product.create({
+      name: 'Unicorn Egg',
+      price: 5000,
+      description: 'A Unicorn of an egg...',
+      image: 'https://i.ytimg.com/vi/KPclvLGx99w/maxresdefault.jpg',
+      stock: 3,
+      origin: 'Equestria',
+      categoryId: 1
+    }),
+    Product.create({
+      name: 'Platypus Egg',
+      price: 55,
+      description: 'The only mammal that lays an egg.',
+      image:
+        'https://qph.fs.quoracdn.net/main-qimg-16bedf49b82147889b1d80bacb310191.webp',
+      stock: 44,
+      origin: 'Australia',
+      categoryId: 2
+    }),
+    Product.create({
+      name: "Yoshi's Egg",
+      price: 1337,
+      description: 'Even you can be mario.',
+      image:
+        'https://cdn.vox-cdn.com/thumbor/CyGR5jAu364WZtRApk3UXbQd-oE=/0x0:876x584/920x613/filters:focal(368x222:508x362):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/62841097/instagram_egg.0.jpg',
+      stock: 2,
+      origin: 'Mushroom Kingdom',
+      categoryId: 1
+    }),
+    Product.create({
+      name: 'Gold Faberge Egg',
+      price: 9999,
+      description: 'Will provide for my grandkids college fund',
+      image:
+        'https://www.religiousitalianart.com/wp-content/uploads/2019/01/faberge-egg-gold.jpg',
+      stock: 1,
+      origin: 'USA',
+      categoryId: 1
     })
   ])
 
