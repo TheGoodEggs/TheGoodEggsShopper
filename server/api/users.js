41ac73e08a70c54ed6eb5bf4711acf1abd99ef76
Adel Jabbar <adeljabb@gmail.com>
2020-01-14 12:21:37 -0500

    created backend routes for user

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+//already mounted on /users
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -13,5 +15,46 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+//add user
+router.post('/', async (req, res, next) => {
+  try {
+    const createdUser = await User.create(req.body)
+    res.send(createdUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//update user
+router.put('/:id', async (req, res, next) => {
+  try {
+    const foundUser = await User.findById(req.params.id)
+    const updatedUser = await foundUser.update({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      address: req.body.address,
+      phone: req.body.phone
+    })
+    res.send(updatedUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//remove user
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.send()
+  } catch (error) {
+    next(error)
   }
 })
