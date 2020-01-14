f6425d0494c3fb118f57be154af96f2eb203cb19
Adam Mak <adammak137@gmail.com>
2020-01-14 12:43:23 -0500

    Changed payment model name -> type, set allowNull to falsee

--- server/db/models/payment.js
+++ server/db/models/payment.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const payment = db.define('payment', {
-  name: {
-    type: Sequelize.String
+  type: {
+    type: Sequelize.String,
+    allowNull: false
   }
 })
 
