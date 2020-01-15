6b5184ec955bf6c4286321b24be214554547f4f0
Tina Fun <tinafun77@gmail.com>
2020-01-15 12:27:19 -0500

    All Product rendering

---
+++ client/components/products.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+export default class AllProducts extends Component {
+  componentDidMount() {
+    this.props.loadProducts()
+  }
+  render() {
+    console.log('WHAT IS products', this.props.products)
+    return (
+      <div>
+        {this.props.products.map(product => {
+          return (
+            <div key={product.id}>
+              <img src={product.image} />
+              <h3>{product.name}</h3>
+              <h4>{product.price}</h4>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
