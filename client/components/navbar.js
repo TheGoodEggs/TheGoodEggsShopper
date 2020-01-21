7fd33e653b83e80ead5146bf91b7873ad83d3159
Tina Fun <tinafun77@gmail.com>
2020-01-21 11:13:27 -0500

    ready to deploy

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -9,14 +9,20 @@ const Navbar = ({handleClick, isLoggedIn}) => (
     <h1>THE EGG SHOPPER</h1>
     <nav>
       {isLoggedIn ? (
-        <div>
+        <div className="navbar">
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-          <Link to="/wishlist">Wishlist</Link>
-          <Link to="/checkout">Checkout</Link>
+          <div className="navright">
+            <Link to="/products">Shop</Link>
+            <Link to="/wishlist">Wishlist</Link>
+            <Link to="/cart">
+              <i className="fa fa-shopping-cart fa-2x" />
+            </Link>
+            <Link to="/checkout">Checkout</Link>
+          </div>
         </div>
       ) : (
         <div className="navbar">
@@ -29,8 +35,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
             <Link to="/cart">
               <i className="fa fa-shopping-cart fa-2x" />
             </Link>
+            <Link to="/checkout">Checkout</Link>
           </div>
-          <Link to="/checkout">Checkout</Link>
         </div>
       )}
     </nav>
