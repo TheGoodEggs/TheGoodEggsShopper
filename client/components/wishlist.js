db5c16372c449d4f65db189513836d50f6fa98d4
Tina Fun <tinafun77@gmail.com>
2020-01-22 17:57:05 -0500

    styling complete

--- client/components/wishlist.js
+++ client/components/wishlist.js
@@ -5,39 +5,41 @@ import {formatMoney} from '../utils'
 class AllWishlist extends React.Component {
   render() {
     return (
-      <div className="allProductsInnerDiv">
-        <Link to={`/products/${this.props.item.id}`}>
-          <img src={this.props.item.image} />
-          <h3>{this.props.item.name}</h3>
-        </Link>
-        <h4>{this.props.item.origin}</h4>
-        <h4>{formatMoney(Number(this.props.item.price))}</h4>
-        {!this.props.item.wishlist ? (
-          <button
-            type="button"
-            onClick={() =>
-              this.props.wishlistHandler.add({
-                id: this.props.user.userId,
-                productId: this.props.item.id
-              })
-            }
-          >
-            <i className="fa fa-heart fa-2x" />
-          </button>
-        ) : (
-          <button
-            type="button"
-            onClick={() =>
-              this.props.wishlistHandler.remove({
-                id: this.props.user.userId,
-                productId: this.props.item.id
-              })
-            }
-          >
-            {' '}
-            <i className="fa fa-heart-o fa-2x" />{' '}
-          </button>
-        )}
+      <div className="allWishOutDiv">
+        <div className="allWishInnerDiv">
+          <Link to={`/products/${this.props.item.id}`}>
+            <img src={this.props.item.image} />
+            <h3>{this.props.item.name}</h3>
+          </Link>
+          {/* <h4>{this.props.item.origin}</h4> */}
+          <p>{formatMoney(Number(this.props.item.price))}</p>
+          {!this.props.item.wishlist ? (
+            <button
+              type="button"
+              onClick={() =>
+                this.props.wishlistHandler.add({
+                  id: this.props.user.userId,
+                  productId: this.props.item.id
+                })
+              }
+            >
+              <i className="fa fa-heart" />
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={() =>
+                this.props.wishlistHandler.remove({
+                  id: this.props.user.userId,
+                  productId: this.props.item.id
+                })
+              }
+            >
+              {' '}
+              <i className="fa fa-heart-o" />{' '}
+            </button>
+          )}
+        </div>
       </div>
     )
   }
